13f7b3e41e1881dc0e676b0830cbfe43c224a92e
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-25 04:27:05 -0300

    Requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
-    return <div data-testid="missions">Missions</div>;
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
   }
 }
 
