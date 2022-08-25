5db562ca593e90e778a178295f8fbc7f33a7f331
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-25 03:10:52 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <Fragment>
+        <Header />
+        <SolarSystem />
+      </Fragment>
     );
   }
 }
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+
+class SolarSystem extends Component {
+  render() {
+    return <div data-testid="solar-system">SolarSystem</div>;
+  }
+}
+
+export default SolarSystem;
