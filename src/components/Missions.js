364d5e1523b36d6b4d1efcda4ad84c6f73f0378a
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-25 04:22:17 -0300

    Requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+
+class Missions extends Component {
+  render() {
+    return <div data-testid="missions">Missions</div>;
+  }
+}
+
+export default Missions;
