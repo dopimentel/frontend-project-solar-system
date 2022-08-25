4c4d1e42edbff2ad38450a77c1ed882e619818a3
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-25 04:16:07 -0300

    Requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {planets.map((planet) => {
+          const { name, image } = planet;
+          return <PlanetCard key={ name } planetName={ name } planetImage={ image } />;
+        })}
       </div>
     );
   }
