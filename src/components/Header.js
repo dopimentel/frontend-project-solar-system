9153d8fb9d2db1d733f10a69fc8308206770a644
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-25 02:55:42 -0300

    Requisito 1

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react/cjs/react.production.min';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
