a3757011fc7b6bfdc99d072d1f539e525b149f45
Marcos Pimentel <mpc_marcos@hotmail.com>
2022-08-25 05:02:58 -0300

    Requisito 10

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Component } from 'react/cjs/react.production.min';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map((mission) => {
+          const { name, year, country, destination } = mission;
+          return (
+            <MissionCard
+              key={ name }
+              name={ name }
+              year={ year }
+              country={ country }
+              destination={ destination }
+            />
+          );
+        })}
       </div>
     );
   }
